7a5bd7632003be693f0b0031cf767e6cf0671c37
Pablo Ifrán <pabloifran@gmail.com>
2023-08-22 15:56:00 -0300

    Fix references to ansible personal
    The repository name changed from ansible-personal to just 
    ansible update all the references that the project had to the old
    repository

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Get started
 
 This repository contains some scripts to start a new computer. To run the scripts
-you first need to follow the instructions on the **Base** section 
- 
+you first need to follow the instructions on the **Base** section
+
 ## Base
 
-This section contains a series of steps for my personal setup, like setup ssh keys 
+This section contains a series of steps for my personal setup, like setup ssh keys
 and things like that, this section can be skipped if you don't need to setup this
 
 ### Requires manual installation
@@ -30,18 +30,18 @@ make install
 
 **Spark (email client)**
 
-Go to the URL [https://sparkmailapp.com/download](https://sparkmailapp.com/download) download the app and 
+Go to the URL [https://sparkmailapp.com/download](https://sparkmailapp.com/download) download the app and
 follow the installation instructions
 
 ### Installed automatically
 
-Navigate to the following 
-[https://github.com/elpic/ansible-personal.git](https://github.com/elpic/ansible-personal.git) 
+Navigate to the following
+[https://github.com/elpic/ansible.git](https://github.com/elpic/ansible.git)
 once there click on the **Code** button and then **Download ZIP**, unzip the file download on your
 Downloads dirctory and run the base-install script to install the base required software, to do
-that you can follow the instructions bellow 
+that you can follow the instructions bellow
 
 ```bash
-cd ~/Downloads/ansible-personal-main
-./bin/base-install.sh 
+cd ~/Downloads/ansible-main
+./bin/base-install.sh
 ```
--- bin/setup.sh
+++ bin/setup.sh
@@ -5,19 +5,19 @@ set -o nounset
 set -o pipefail
 
 if [[ "${TRACE-0}" == "1" ]]; then
-  set -o xtrace
+	set -o xtrace
 fi
 
 if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
-  echo "Usage ./bin/setup.sh [OPTIONS]"
-  echo ""
-  echo "If --ask-vault-pass is not passed then ssh tag is going to be skip"
-  echo ""
-  echo "Options:"
-  echo ""
-  echo "  -t [tags] - a comma separated list that is going to be used to filter what to install"
-  echo "  --ask-vault-pass - ask you the password to decrypt the encrypted files"
-  exit
+	echo "Usage ./bin/setup.sh [OPTIONS]"
+	echo ""
+	echo "If --ask-vault-pass is not passed then ssh tag is going to be skip"
+	echo ""
+	echo "Options:"
+	echo ""
+	echo "  -t [tags] - a comma separated list that is going to be used to filter what to install"
+	echo "  --ask-vault-pass - ask you the password to decrypt the encrypted files"
+	exit
 fi
 
 # Install base software used
@@ -26,26 +26,25 @@ ANSIBLE_ARGS=""
 
 # Ask for password if the ask-vault-pass is given
 if [[ "$*" == *"--ask-vault-pass"* ]]; then
-  unset -v PASSWORD # make sure it's not exported
-  set +o allexport  # make sure variables are not automatically exported
-  IFS= read -rsp 'Please enter password for vault: ' PASSWORD < /dev/tty
+	unset -v PASSWORD # make sure it's not exported
+	set +o allexport  # make sure variables are not automatically exported
+	IFS= read -rsp 'Please enter password for vault: ' PASSWORD </dev/tty
 
-  TEMPFILE=$(mktemp)
+	TEMPFILE=$(mktemp)
 
-  echo "$PASSWORD" >> $TEMPFILE
-  ANSIBLE_ARGS="--vault-password-file=$TEMPFILE"
-  echo ""
+	echo "$PASSWORD" >>$TEMPFILE
+	ANSIBLE_ARGS="--vault-password-file=$TEMPFILE"
+	echo ""
 else
-  # This is to prevent errors since the password was not provided
-  ANSIBLE_ARGS="--skip-tags ssh,aws"
+	# This is to prevent errors since the password was not provided
+	ANSIBLE_ARGS="--skip-tags ssh,aws"
 fi
 
 # Remove the option because is going to be provided in a tempfile
-for arg do
-  shift
-  [ "$arg" = "--ask-vault-pass" ] && continue
-  set -- "$@" "$arg"
+for arg; do
+	shift
+	[ "$arg" = "--ask-vault-pass" ] && continue
+	set -- "$@" "$arg"
 done
 
-ansible-pull -U https://github.com/elpic/ansible-personal.git $@ $ANSIBLE_ARGS
-
+ansible-pull -U https://github.com/elpic/ansible.git $@ $ANSIBLE_ARGS
